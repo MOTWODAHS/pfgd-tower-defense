e350fab42589f45fa0f4bc803de9fd8b802bd199
MOTWODAHS <duxuhao521123@gmail.com>
2020-05-04 22:58:23 -0400

    Add Game Over
    Final Playable

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -16,6 +16,11 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (GameManager.GameIsOver)
+        {
+            this.enabled = false;
+        }
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             doMovement = !doMovement;
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-    private bool gameEnded = false;
+    public static bool GameIsOver;
+
+    public GameObject gameOverUI;
+
+    void Start()
+    {
+        GameIsOver = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameEnded)
+        if (GameIsOver)
         {
             return;
         }
@@ -23,7 +30,7 @@ public class GameManager : MonoBehaviour
 
     void EndGame()
     {
-        gameEnded = true;
-        Debug.Log("Game Over");
+        GameIsOver = true;
+        gameOverUI.SetActive(true);
     }
 }
